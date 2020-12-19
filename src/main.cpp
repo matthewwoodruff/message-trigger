0c804d12b61109cb28b9867812b1e79af3d7d64f
Matthew Woodruff <mwoodruf@thoughtworks.com>
2020-12-19 00:01:18 +0000

    Refactor usinh for loops

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,36 @@
 
 int MINUTES_BETWEEN_MESSAGES = 10;
 
-int MESSAGE_1 = 2;
-int MESSAGE_2 = 3;
-int MESSAGE_3 = 4;
-int MESSAGE_4 = 5;
+int MESSAGE_1 = 3;
+int MESSAGE_2 = 5;
+int MESSAGE_3 = 7;
+int MESSAGE_4 = 9;
+
+#define NUMBER_OF_MESSAGES 4
+
+int messages[NUMBER_OF_MESSAGES] = {MESSAGE_1, MESSAGE_2, MESSAGE_3, MESSAGE_4};
 
 void setup() {
-  pinMode(MESSAGE_1, OUTPUT);
-  pinMode(MESSAGE_2, OUTPUT);
-  pinMode(MESSAGE_3, OUTPUT);
-  pinMode(MESSAGE_4, OUTPUT);
+  for (int i = 0; i < NUMBER_OF_MESSAGES; i++) {
+    pinMode(messages[i], OUTPUT);
+  } 
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void trigger(int message) {
+void flash() {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+}
+
+void trigger(int message, int flashes) {
+  for (int i = 0; i < flashes; i++) {
+    flash();
+  }
   digitalWrite(message, HIGH);   
-  delay(200);                      
-  digitalWrite(message, LOW);    
+  delay(500);                      
+  digitalWrite(message, LOW);   
 }
 
 void wait(int minutes) {
@@ -25,15 +39,8 @@ void wait(int minutes) {
 }
 
 void loop() {
-  trigger(MESSAGE_1);   
-  wait(MINUTES_BETWEEN_MESSAGES);
-
-  trigger(MESSAGE_2);   
-  wait(MINUTES_BETWEEN_MESSAGES); 
-
-  trigger(MESSAGE_3);   
-  wait(MINUTES_BETWEEN_MESSAGES); 
-
-  trigger(MESSAGE_4);   
-  wait(MINUTES_BETWEEN_MESSAGES);                       
+  for (int i = 0; i < NUMBER_OF_MESSAGES; i++) {
+    trigger(messages[i], i + 1);   
+    wait(MINUTES_BETWEEN_MESSAGES);
+  }                    
 }
